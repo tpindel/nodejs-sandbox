7111bfeb61dc8f741dbef41537657a92d7cf9918
tpindel <pindel.tomasz@gmail.com>
2015-02-06 10:59:11 +0100

    Task 7 - create http server

--- nodeschol.io/streams/program-7.js
+++ nodeschol.io/streams/program-7.js
@@ -5,9 +5,11 @@ var server = http.createServer(function (req, res) {
   if (req.method === 'POST') {
     req.pipe(tr(function(data){
       this.queue(data.toString().toUpperCase());
+    }, function end() {
+      this.queue(null);
     })).pipe(res);
   } else {
-    res.end();
+    res.end('end\n');
   }
 });
-server.listen(process.argv[2]);
+server.listen(process.argv[2] || 8000);
